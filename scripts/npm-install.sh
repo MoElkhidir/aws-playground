078bbaae205a649b54ce435f5bcef043420ca122
Mohamed Elkhidir <mhharktat@gmail.com>
2020-03-22 13:39:01 +0100

    Update npm-install.sh

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export NVM_DIR="home/ubuntu/.nvm"
+
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo 'mo'
 echo $NVM_DIR
-su - ubuntu -c "npm install"
+sudo npm install
 
